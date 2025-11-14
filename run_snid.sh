6cee08f6bf20d7744c214811d680d8de4a0adf93
Mateusz Bronikowski <37741410+mbronikowski@users.noreply.github.com>
2025-11-14 15:27:21 +0100

    Update for mac

--- run_snid.sh
+++ run_snid.sh
@@ -1,52 +1,49 @@
 #!/bin/bash
-# Cross-platform wrapper for SNID Docker container with X11 forwarding
-# Works on Linux and macOS (XQuartz)
-# Only revokes xhost permissions if the script added them
+# Working macOS + Linux wrapper for SNID with XQuartz
 
 ADDED_XHOST=0
-
-# Detect platform
-OS="$(uname -s)"
+OS=$(uname -s)
 
 if [[ "$OS" == "Darwin" ]]; then
-    # macOS (XQuartz)
     DISPLAY_VAR="host.docker.internal:0"
 
-    # Add xhost permission if not present
-    if ! xhost | grep -q "127.0.0.1"; then
-        xhost +127.0.0.1 >/dev/null
-        ADDED_XHOST=1
+    # launch XQuartz if needed
+    if ! pgrep XQuartz >/dev/null; then
+        open -a XQuartz
+        sleep 2
     fi
+
+    # XQuartz needs network access or Docker can't connect
+    defaults write org.xquartz.X11 enable_iglx -bool true
+    defaults write org.xquartz.X11 no_auth -bool true
+
+    xhost +127.0.0.1
+    ADDED_XHOST=1
 
 elif [[ "$OS" == "Linux" ]]; then
-    # Linux
     DISPLAY_VAR="$DISPLAY"
+    xhost +local:docker
+    ADDED_XHOST=1
 
-    if ! xhost | grep -q "local:docker"; then
-        xhost +local:docker >/dev/null
-        ADDED_XHOST=1
-    fi
 else
     echo "Unsupported OS: $OS"
     exit 1
 fi
 
-# Cleanup function for xhost
 cleanup() {
     if [[ "$ADDED_XHOST" -eq 1 ]]; then
         if [[ "$OS" == "Darwin" ]]; then
-            xhost -127.0.0.1 >/dev/null
-        elif [[ "$OS" == "Linux" ]]; then
-            xhost -local:docker >/dev/null
+            xhost -127.0.0.1
+        else
+            xhost -local:docker
         fi
     fi
 }
 trap cleanup EXIT
 
-# Run Docker container
 docker run --rm -it \
     -e DISPLAY="$DISPLAY_VAR" \
-    $( [[ "$OS" == "Linux" ]] && echo "-v /tmp/.X11-unix:/tmp/.X11-unix" ) \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v "$PWD":/home/sniduser/workdir \
     -w /home/sniduser/workdir \
     snid:5.0-supersnid \
